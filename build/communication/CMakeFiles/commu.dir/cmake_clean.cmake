7b6420c6b96847055652a797116b5cbe8f1b03f8
itsnoteasytonameaccount <whatasillyfishtwo@gmail.com>
2021-08-28 02:40:16 +0800

    cmake

---
+++ build/communication/CMakeFiles/commu.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/commu.dir/communication.cpp.obj"
+  "libcommu.dll"
+  "libcommu.dll.a"
+  "libcommu.dll.manifest"
+  "libcommu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/commu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
